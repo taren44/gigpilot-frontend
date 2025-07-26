b8596e54a465aabbc7df5506ad694f359b0b8111
Taren Teno <tarenteno@Tarens-MacBook-Pro.local>
2025-07-27 04:07:26 +0530

    Fix React startup in main.tsx

--- main.tsx
+++ main.tsx
@@ -1,11 +1,16 @@
-import "./index.css";
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} else {
+  console.error('Vite app could not find the root element!');
+}
